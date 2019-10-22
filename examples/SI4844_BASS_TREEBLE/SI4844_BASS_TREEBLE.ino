fc6d5ca9dec0788d7077725fd671c3bf5a83425f
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2019-10-22 08:56:48 -0300

    Examples

---
+++ examples/SI4844_BASS_TREEBLE/SI4844_BASS_TREEBLE.ino
@@ -0,0 +1,64 @@
+/*
+ * SI4844 radio. Minimal Sketch.  
+ * By Ricardo Lima Caratti (PU2CLR), Oct, 2019.
+ */
+
+#include <SI4844.h>
+// Tested on Arduino Pro Mini
+#define INTERRUPT_PIN 2  
+#define RESET_PIN 12
+// Pages 17 and 18 from Si48XX ATDD PROGRAMMING GUIDE
+#define DEFAULT_BAND 4   // FM => 0 to 19; AM => 20 to 24; SW => 25 to 40
+
+SI4844 si4844; 
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Bass, treble and volume test.");
+  delay(500);  
+
+  si4844.setup(RESET_PIN, INTERRUPT_PIN, DEFAULT_BAND);
+  si4844.setVolume(55); // It can be from 0 to 63.
+
+  // See Si48XX ATDD PROGRAMMING GUIDE, page 21
+  si4844.setAudioMode(2,0,0,0,0);
+
+}
+
+void loop() {
+  char key = Serial.read();
+  switch (key)
+  {
+    case 'b':
+    case 'B': 
+      si4844.bassTrebleDown();
+      break;
+    case 't':
+    case 'T':
+      si4844.bassTrebleUp();
+      break;
+    case '+': // sound volume control
+      si4844.volumeUp();
+      break;
+    case '-':
+      si4844.volumeDown();
+      break;        
+    default: 
+      break;  
+  }
+    
+  // If you move the tuner, hasStatusChanged returns true
+  if (si4844.hasStatusChanged())
+  {
+    Serial.print("[Band..: ");
+    Serial.print(si4844.getBandMode());
+    Serial.print(" - Frequency: ");    
+    Serial.print(si4844.getFrequency(),0);
+    Serial.print(" KHz");
+    if (si4844.getStatusBandMode() == 0) {
+      Serial.print(" - Stereo ");
+      Serial.print(si4844.getStereoIndicator());
+    }
+    Serial.println("]");
+  }
+}
