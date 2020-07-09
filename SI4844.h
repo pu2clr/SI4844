e2f39070d9cbd890f7b49fc29289b94e3e9466cf
Ricardo Lima Caratti <ricardo.caratti@adm.cogect>
2020-07-09 10:17:00 -0300

    SI4844 Arduino Library

--- SI4844.h
+++ SI4844.h
@@ -179,20 +179,25 @@ typedef union {
 } si4844_property;
 
 /**
- * @brief 
- * 
- * Handling interruptions.
- * Whenever the status of the ATDD changes, a hardware interrupt is triggered. For example, when you move the tuner
- * potenciometer, the ATDD sends a signal to the Arduino pin (INTERRUPT_PIN). The same is true when the ATDD is capturing 
- * mono FM signal and has switched to stereo. The variable below indicates a change of the ATDD status. It will need to  
- * process some action (for example show on LCD this changes).   
+ * @brief Interrupt status
+ * @details This variable must be true every time an interruption occurs and then must return to the false every time an action resulting from the interruption is performed.
+ * @details The variable below indicates a change of the ATDD status. When it occurs, it means the system needs to process some action (for example show on LCD this change).   
  */
 volatile static bool data_from_device;
 
+/**
+ * @brief Library handle interrupt
+ * @details Handling interruptions.
+ * @details Whenever the status of the ATDD changes, a hardware interrupt is triggered. For example, when you move the tuner
+ * @details potenciometer, the ATDD sends a signal to the Arduino pin (INTERRUPT_PIN). The same is true when the ATDD is capturing 
+ * @details mono FM signal and has switched to stereo. 
+ * @details You can control the interrupt process via your sketch intead of this library. 
+ * @see setStatusInterruptFromDevice, getStatusInterruptFromDevice, setup 
+ */
+
 static void interrupt_hundler()
 {
- 
-  data_from_device = true;
+   data_from_device = true;
 }
 
 
