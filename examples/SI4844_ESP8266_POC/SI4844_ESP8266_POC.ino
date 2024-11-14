9a60c5f114548e1ca93fbb06a72e209488495549
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-11-13 22:17:46 -0300

    Doc

--- examples/SI4844_ESP8266_POC/SI4844_ESP8266_POC.ino
+++ examples/SI4844_ESP8266_POC/SI4844_ESP8266_POC.ino
@@ -1,14 +1,15 @@
 /*
- * It is a Proof of Concept (POC) of using Si4844 with the Arduino Library for rx.
+ *  UNDER CONSTRUCTION... 
+ *  It is a Proof of Concept (POC) of using Si4844 with the Arduino Library for rx.
  * 
- *  SI4844 and ESP32C3 connections
+ *  SI4844 and ESP8266 connections
  *  
- *  | SI4844 pin |  ESP32C3 pin |  Description                                       |
+ *  | SI4844 pin |  Arduino pin |  Description                                       |
  *  | ---------  | ------------ | -------------------------------------------------  |
- *  |    2       |   5          | Interrupt pin                                      |
- *  |   15       |   6          | Regurlar digital pin used to RESET control         |
- *  |   16       |   8 (SDA)    | I2C bus (Data)                                     |
- *  |   17       |   9 (SCL)    | I2C bus (Clocl)                                    |
+ *  |    2       | GPIO2        | Interrupt pin                                      |
+ *  |   15       | GPIO12       | Regurlar digital pin used to RESET control         |
+ *  |   16       | GPIO4(SDA)   | I2C bus (Data)                                     |
+ *  |   17       | GPIO5(SCL)   | I2C bus (Clocl)                                    |
  * 
  *  Install and use the esptool.py
  *  Run the command "esptool.py flash_id"  to get information about your board. 
@@ -19,12 +20,12 @@
  */
 #include <SI4844.h>
 // Arduino Pin (tested on pro mini)
-#define INTERRUPT_PIN 5
-#define RESET_PIN     6
+#define INTERRUPT_PIN 2
+#define RESET_PIN    15
 #define DEFAULT_BAND  1
 
-#define I2C_SDA       8
-#define I2C_SCL       9
+#define I2C_SDA       4
+#define I2C_SCL       5
 
 SI4844 rx; 
 
@@ -32,7 +33,7 @@ void setup() {
   Serial.begin(9600);
   delay(1000);
 
-  Serial.print(F("\nESP32C3 - Begin...\n"));
+  Serial.print( "\ESP8266 - Begin...\n ");
   Serial.flush();
 
   Wire.begin(I2C_SDA, I2C_SCL);
@@ -48,51 +49,51 @@ void setup() {
 }
 // Shows instruções
 void instructions() {
-  Serial.println(F("-------------------ESP32C3---CONTROLLER------------------"));
-  Serial.println(F("Type F - FM; A - AM; 1 to 7 - SW1 to SW7"));
-  Serial.println(F("Type f - Custom FM - from 77 to 109 MHz - Step 200kHz"));
-  Serial.println(F("Type h - Custom FM - from 101 to 104 MHz - Step 200kHz"));
-  Serial.println(F("Type + or - to sound volume"));  
-  Serial.println(F("Type B to Bass; T to Treeble and N to Normal"));  
-  Serial.println(F("Type c - custom band 5.7 to 6.2 MHz"));
-  Serial.println(F("Type C - CB (custom Band 27.0 to 27.5 MHz"));
-  Serial.println(F("Type o to Power Down"));
-  Serial.println(F("Type I to Firmware Information"));
-  Serial.println(F("---------------------------------------------------"));
+  Serial.println( "-------------------ESP8266---CONTROLLER------------------ ");
+  Serial.println( "Type F - FM; A - AM; 1 to 7 - SW1 to SW7 ");
+  Serial.println( "Type f - Custom FM - from 77 to 109 MHz - Step 200kHz ");
+  Serial.println( "Type h - Custom FM - from 101 to 104 MHz - Step 200kHz ");
+  Serial.println( "Type + or - to sound volume ");  
+  Serial.println( "Type B to Bass; T to Treeble and N to Normal ");  
+  Serial.println( "Type c - custom band 5.7 to 6.2 MHz ");
+  Serial.println( "Type C - CB (custom Band 27.0 to 27.5 MHz ");
+  Serial.println( "Type o to Power Down ");
+  Serial.println( "Type I to Firmware Information ");
+  Serial.println( "--------------------------------------------------- ");
   delay(500);
 }
 // Shows firmware information
 void show_firmware_information() {
-  Serial.println(F("\nSI4844 -  Firmware information\n"));
+  Serial.println( "\nSI4844 -  Firmware information\n ");
   rx.getFirmware();
-  Serial.print(F("Final 2 digits of Part Number..: "));
+  Serial.print( "Final 2 digits of Part Number..:  ");
   Serial.println(rx.getFirmwarePartNumber(), HEX);
-  Serial.print(F("Firmware Major Revision........: "));
+  Serial.print( "Firmware Major Revision........:  ");
   Serial.println(rx.getFirmwareMajorRevision());
-  Serial.print(F("Firmware Minor Revision........: "));
+  Serial.print( "Firmware Minor Revision........:  ");
   Serial.println(rx.getFirmwareMinorRevision());
-  Serial.print(F("Component Major Revision.......: "));
+  Serial.print( "Component Major Revision.......:  ");
   Serial.println(rx.getFirmwareComponentMajorRevision());
-  Serial.print(F("Component Minor Revision.......: ")); 
+  Serial.print( "Component Minor Revision.......:  "); 
   Serial.println(rx.getFirmwareComponentMinorRevision());
-  Serial.print(F("Chip Revision..................: "));
+  Serial.print( "Chip Revision..................:  ");
   Serial.println(rx.getFirmwareChipRevision());
-  Serial.println(F("*****************************"));
+  Serial.println( "***************************** ");
 }
 
 void showStatus() {
-    Serial.print(F("Band Index: "));
+    Serial.print( "Band Index:  ");
     Serial.print(rx.getStatusBandIndex());
-    Serial.print(F(" - "));
+    Serial.print( " -  ");
     Serial.print(rx.getBandMode());
-    Serial.print(F(" - Frequency: "));    
+    Serial.print( " - Frequency:  ");    
     Serial.print(rx.getFrequency(),0);
-    Serial.print(F(" KHz"));
+    Serial.print( " KHz ");
     if (rx.getStatusBandMode() == 0) {
-      Serial.print(F(" - Stereo "));
+      Serial.print( " - Stereo  ");
       Serial.print(rx.getStereoIndicator());
     }
-    Serial.print(F(" - Volume: "));
+    Serial.print( " - Volume:  ");
     Serial.print(rx.getVolume());
     Serial.println("");  
 }
@@ -111,11 +112,11 @@ void loop() {
       rx.setBand(1); // FM band
       break;
     case 'f': 
-      Serial.println(F("Custom FM Band:  from to 77 to 109 MHz - Step 200 kHz"));
+      Serial.println( "Custom FM Band:  from to 77 to 109 MHz - Step 200 kHz ");
       rx.setCustomBand(3,7700,10900,20);    
       break;
     case 'h': 
-      Serial.println(F("Custom FM Band:  from to 101 to 104 MHz - Step 200 kHz"));
+      Serial.println( "Custom FM Band:  from to 101 to 104 MHz - Step 200 kHz ");
       rx.setCustomBand(3,10100,10400,20);  
       break;    
     case 'a':
@@ -150,20 +151,20 @@ void loop() {
       rx.volumeDown();
       break;  
     case 'o':
-       Serial.println(F("Power Down"));
+       Serial.println( "Power Down ");
        delay(500); 
       rx.powerDown();
       break;  
     case 'c':
       // Configure the Pre-defined Band (band index 26) to work between 5.7 to 6.2 MHz
       // See Si48XX ATDD PROGRAMMING GUIDE, pages 17,18,19 and 20.
-      Serial.println(F("Custom Band:  5.7 to 6.2 MHz"));
+      Serial.println( "Custom Band:  5.7 to 6.2 MHz ");
       rx.setCustomBand(26,5700,6200,5);  
       break;      
     case 'C': 
       // Configure the Pre-defined Band (band index 40) to work between 27.0 to 27.5 MHz
       // See Si48XX ATDD PROGRAMMING GUIDE, pages 17,18,19 and 20.
-      Serial.println(F("Custom Band: 27.0 to 27.5 MHz"));
+      Serial.println( "Custom Band: 27.0 to 27.5 MHz ");
       rx.setCustomBand(40,27000,27500,5);  
       break;  
     case 'I': 
